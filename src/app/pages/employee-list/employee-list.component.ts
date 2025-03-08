2796547ba92177ac3aa438c9511a55afa47a901c
Raghav-0007 <rmyadav514@gmail.com>
2025-03-08 13:39:35 +0530

    [Radhamohan yadav] . Add created employee-list component

--- src/app/pages/employee-list/employee-list.component.ts
+++ src/app/pages/employee-list/employee-list.component.ts
@@ -1,11 +1,42 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common'; 
 
 @Component({
   selector: 'app-employee-list',
-  imports: [],
+  imports: [RouterModule, CommonModule],
   templateUrl: './employee-list.component.html',
   styleUrl: './employee-list.component.css'
 })
 export class EmployeeListComponent {
-
+  employees = [
+    { 
+      id: '1',
+      name: 'Amarpa Kumar', 
+      gender: 'Female', 
+      departments: ['Sales', 'HR', 'Finance'], 
+      salary: '₹ 10,000', 
+      startDate: '29 Oct 2019', 
+      image: 'assets/dp1.jpeg' 
+    },
+    { 
+      id: '2',
+      name: 'Mohammad Shaikh', 
+      gender: 'Female', 
+      departments: ['Sales', 'HR', 'Finance'], 
+      salary: '₹ 10,000', 
+      startDate: '29 Oct 2019', 
+      image: 'assets/dp2.jpeg'  
+    },
+    { 
+      id: '3',
+      name: 'Bubere Qais', 
+      gender: 'Male', 
+      departments: ['Sales', 'HR', 'Finance'], 
+      salary: '₹ 10,000', 
+      startDate: '29 Oct 2019', 
+      image: 'assets/dp3.jpeg'
+    }
+  ];
+  
 }
